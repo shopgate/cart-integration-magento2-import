a9cecee1953598ac98ea4e59c0d28a33644e1a03
Konstantin Kiritsenko <k@componentk.com>
2018-06-28 03:55:26 +0300

    Fixing deployment zip so that we can upload the file to marketplace directly
    Signed-off-by: Konstantin Kiritsenko <k@componentk.com>

--- release/build_release_package.sh
+++ release/build_release_package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ZIP_FOLDER="shopgate_import-${TRAVIS_TAG}"
+ZIP_FOLDER="shopgate-import-${TRAVIS_TAG}"
 ZIP_FILE="${ZIP_FOLDER}.zip"
 
 # Remove mentioning the src/ structure when packaging
@@ -11,8 +11,8 @@ sed -i 's/cart-integration-magento2-//g' composer.json
 mkdir release/${ZIP_FOLDER}
 rsync -a ./src/ release/${ZIP_FOLDER}
 rsync -av --exclude-from './release/exclude-list.txt' ./ release/${ZIP_FOLDER}
-cd ./release
-zip -r ${ZIP_FILE} ${ZIP_FOLDER}
+cd ./release/${ZIP_FOLDER}
+zip -r ${ZIP_FILE} ./
 wget https://raw.githubusercontent.com/magento/marketplace-tools/master/validate_m2_package.php
 php validate_m2_package.php ${ZIP_FILE}
 mv ${ZIP_FILE} ${TRAVIS_BUILD_DIR}/${ZIP_FILE}
