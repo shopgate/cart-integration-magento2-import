469eb33050159344255c7b61fb63c26d109d2577
André Kraus <andre.kraus@shopgate.com>
2019-07-08 10:12:28 +0200

    Magento2 331 wrong shipping costs (#18)
    * MAGENTO2-331: force fallback for shipping
    
    * MAGENTO2-331: improved shipping mapping
    
    * MAGENTO2-331: fixed change log

--- src/Helper/Quote.php
+++ src/Helper/Quote.php
@@ -59,16 +59,19 @@ class Quote extends \Shopgate\Base\Helper\Quote
      */
     protected function setShipping()
     {
+        $this->quoteRepository->save($this->quote);
+
         $this->setItemQty();
         $client     = is_null($this->sgBase->getClient()) ? '' : $this->sgBase->getClient()->getType();
         $methodName = $this->sgBase->getShippingInfos()->getName();
         $rate       = $this->quote->getShippingAddress()
-                                  ->setCollectShippingRates(true)
-                                  ->collectShippingRates()
-                                  ->getShippingRateByCode($methodName);
+            ->setCollectShippingRates(true)
+            ->collectShippingRates()
+            ->getShippingRateByCode($methodName);
+        
         $this->quote->getShippingAddress()
-                    ->setShippingMethod($rate ? $methodName : 'shopgate_fix')
-                    ->setData(OrderCondition::CLIENT_ATTRIBUTE, $client);
+            ->setShippingMethod($rate ? $methodName : 'shopgate_fix')
+            ->setData(OrderCondition::CLIENT_ATTRIBUTE, $client);
     }
 
     /**
